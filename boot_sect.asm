48a21faff6e290c4c6763a690cc4ed5143fe84bb
Anand J <anand.indukala@gmail.com>
2016-03-05 03:39:46 +0530

    Stack manipulation using push and pop

--- boot_sect.asm
+++ boot_sect.asm
@@ -5,29 +5,27 @@
 
 mov ah, 0x0e	; tele-type mode
 
-; First attempt
-mov al, the_secret
+mov bp, 0x8000	; Setting the base of stack
+mov sp, bp
+
+push 'A'	; Pushing characters to the stack
+push 'B'
+push 'C'
+
+pop bx		; we can only pop 16-bits, so pop to bx
+mov al, bl	; then copy bl (8-bit) to al
 int 0x10
 
-; Second attempt
-mov al, [the_secret]
+pop bx		; we can only pop 16-bits, so pop to bx
+mov al, bl	; then copy bl (8-bit) to al
 int 0x10
 
-; Third attempt
-mov bx, the_secret
-add bx, 0x7c00
-mov al, [bx]
-int 0x10
-
-; Forth attempt
-mov al, [0x7c1e]
+mov al, [0x7ffe]; To prove our stack grows downwards from bp,
+		; fetch the char at 0x8000 - 0x2 (ie, 16-bits)
 int 0x10
 
 
 jmp $		; jump to the current address. meaning eternity.
-
-the_secret:
-  db "X"
 
 
 times 510-($-$$) db 0	; Padding with zeros
