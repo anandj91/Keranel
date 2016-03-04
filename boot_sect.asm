c85995083c4d4600316d75be84b9c95a56536264
Anand J <anand.indukala@gmail.com>
2016-03-05 02:54:37 +0530

    Printing Hello at start

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,6 +1,22 @@
-loop:
-  jmp loop
+;
+; Simple boot sector that prints a message to the screen using BIOS routine
+;
 
-times 510-($-$$) db 0
+mov ah, 0x0e	; tele-type mode
 
-dw 0xaa55
+mov al, 'H'	; Setting 'H' for printing on the screen
+int 0x10	; Giving 0x10 interrupt for printing the character in al registry
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $		; jump to the current address. meaning eternity.
+
+times 510-($-$$) db 0	; Padding with zeros
+
+dw 0xaa55	; Magic number
