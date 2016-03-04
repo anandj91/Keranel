9268624bcc52132ea33c800c625f8b4f9d03e1b2
Anand J <anand.indukala@gmail.com>
2016-03-05 03:19:04 +0530

    Find the Byte game

--- boot_sect.asm
+++ boot_sect.asm
@@ -4,18 +4,30 @@
 
 mov ah, 0x0e	; tele-type mode
 
-mov al, 'H'	; Setting 'H' for printing on the screen
-int 0x10	; Giving 0x10 interrupt for printing the character in al registry
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
+; First attempt
+mov al, the_secret
 int 0x10
 
+; Second attempt
+mov al, [the_secret]
+int 0x10
+
+; Third attempt
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
+int 0x10
+
+; Forth attempt
+mov al, [0x7c1e]
+int 0x10
+
+
 jmp $		; jump to the current address. meaning eternity.
+
+the_secret:
+  db "X"
+
 
 times 510-($-$$) db 0	; Padding with zeros
 
