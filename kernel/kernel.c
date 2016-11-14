d9d1518c188431e8a3dd33055c74fb03dc8b3cbf
Anand J <anand.indukala@gmail.com>
2016-11-14 15:41:23 +0530

    Video driver

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,12 +1,19 @@
-void main(){
-  /*
-  * Create a pointer to a char, and point it to the first text cell of
-  * video memory (ie, top left of the screen)
-  */
-  char* video_memory = (char*) 0xb8000;
-  /*
-   * At the address pointed to by video memory, store the character 'X'
-   * (ie, display 'X' in the top left of the screen)
-   */
-  *video_memory = 'X';
+void write_string(int color, const char* str);
+
+void main()
+{
+    write_string(10, "First driver");
+}
+
+void write_string(int color, const char* str)
+{
+    volatile char* video_memory = (char*) 0xb8000;
+
+    while(*str != '\0') {
+        *video_memory = *(str);
+        str++;
+        video_memory++;
+        *video_memory = color;
+        video_memory++;
+    }
 }
